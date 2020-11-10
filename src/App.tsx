fd79d4ff5cb4ffb3d1a14e044134135f3565efdf
SASAKI Shunsuke <erukiti@gmail.com>
2020-11-11 00:17:19 +0900

    テストメッセージ送信

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={() => window.ipc.test("Hello, Electron Main World.")}>
+      テストメッセージ送信
+    </button>
   );
 }
 
