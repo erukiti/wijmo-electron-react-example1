bf396a49aaa2ba9046b41c7f1d80959ac134eeb7
SASAKI Shunsuke <erukiti@gmail.com>
2020-11-11 00:18:45 +0900

    Wijmo + React + Electron

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 // src/App.tsx
 import React from "react";
+import "@grapecity/wijmo.styles/wijmo.css"; // (1)
+
+import * as wjGauge from "@grapecity/wijmo.react.gauge"; // (2)
 
 function App() {
+  const [gauge, setGauge] = React.useState(30); // (3)
   return (
-    <button onClick={() => window.ipc.test("Hello, Electron Main World.")}>
-      テストメッセージ送信
-    </button>
+    <div>
+      <h1>Wijmo + React + Electron</h1>
+      <wjGauge.LinearGauge // (4)
+        className="wijmo-control" // (5)
+        isReadOnly={false} // (6)
+        value={gauge} // (7)
+        valueChanged={(ev: any) => setGauge(ev.value)} // (8)
+      />
+      ゲージの値: {gauge} {/* (9) */}
+    </div>
   );
 }
 
